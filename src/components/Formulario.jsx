fdb5f33031ce3b7382afac80c21b26027cc5baed
Carlos Zambrano <cmzambra@espol.edu.ec>
2023-06-14 23:56:54 -0500

    Integracion AWS

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,131 @@
+import Swal from "sweetalert2";
+import { useState } from "react";
+import Factura from "./Factura";
+
+const Fomulario = () => {
+    const [toDo, setTodo] = useState({
+        title: "",
+        description: "",
+        state: "Pendiente",
+        priority: true,
+    });
+
+    const { title, description, state, priority } = toDo;
+    const handelSubmit = e => {
+        e.preventDefault();
+        if (!title.trim() || !description.trim()) {
+            return Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Titulo y descripción obligatorios",
+            });
+        } else {
+            addToDo({
+                id: Date.now(),
+                ...toDo,
+                state: state === "Completado",
+            });
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "ToDo agregado correctamente",
+                showConfirmButton: false,
+                timer: 1500,
+            });
+        }
+    };
+
+    const handleChange = e => {
+        const { name, type, checked, value } = e.target;
+        setTodo({
+            ...toDo,
+            [name]: type === "checkbox" ? checked : value,
+        });
+
+        if (type === "checkbox") {
+            const facturaAdicional = document.getElementById("datosFactura");
+            checked
+                ? facturaAdicional.classList.add("d-none")
+                : facturaAdicional.classList.remove("d-none");
+        }
+    };
+
+    return (
+        <div className="container">
+            <h2 className="my-4">Facturación y Entrega</h2>
+            <form className="" onSubmit={handelSubmit}>
+                <label htmlFor="">Cliente:</label>
+                <input
+                    className="form-control mb-2"
+                    placeholder="Juan Leon"
+                    type="text"
+                    name="client"
+                    value={title}
+                    onChange={handleChange}
+                />
+                <label htmlFor="">Cedula de Identidad:</label>
+                <input
+                    className="form-control mb-2"
+                    placeholder="13157093343"
+                    type="text"
+                    pattern="\d*"
+                    maxLength="10"
+                    name="identification"
+                    value={title}
+                    onChange={handleChange}
+                />
+                <label htmlFor="">Correo Electrónico:</label>
+                <input
+                    className="form-control mb-2"
+                    placeholder="example@mail.com"
+                    type="email"
+                    name="mail"
+                    value={title}
+                    onChange={handleChange}
+                />
+                <label htmlFor="">Teléfono móvil:</label>
+                <input
+                    className="form-control mb-2"
+                    placeholder="09934345345"
+                    type="text"
+                    pattern="\d*"
+                    maxLength="10"
+                    name="phone"
+                    value={title}
+                    onChange={handleChange}
+                />
+                <label htmlFor="">Dirección de Entrega:</label>
+                <input
+                    className="form-control mb-2"
+                    placeholder="Guayaquil, Cdla: Alborada"
+                    type="text"
+                    name="direction"
+                    value={title}
+                    onChange={handleChange}
+                />
+                <div className="form-check mb-2">
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="inputCheck"
+                        name="factura"
+                        checked={priority}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="inputCheck">
+                        ¿Usar los mismos datos para la facturación?
+                    </label>
+                </div>
+                <div className="d-none" id="datosFactura">
+                    <h2 className="my-4">Datos para la facturación</h2>
+                    <Factura />
+                </div>
+            </form>
+            <button className="btn btn-primary" type="submit">
+                Resumen del pedido
+            </button>
+        </div>
+    );
+};
+
+export default Fomulario;
---
+++ src/components/S3Uploader.jsx
@@ -0,0 +1,66 @@
+// ---------------------------------------------------------
+//  ==> Import de Credenciales de AWS
+// ---------------------------------------------------------
+import {
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_REGION,
+    AWS_SESSION_TOKEN,
+    AWS_BUCKET_NAME,
+} from "../../credentials-aws.json";
+
+// ---------------------------------------------------------
+//  ==> Import de las dependencias
+// ---------------------------------------------------------
+import React from "react";
+import AWS from "aws-sdk";
+import { useState } from "react";
+
+// ---------------------------------------------------------
+
+AWS.config.update({
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    region: AWS_REGION,
+    sessionToken: AWS_SESSION_TOKEN,
+});
+
+export const S3Uploader = () => {
+    const s3 = new AWS.S3();
+    const [imageUrl, setImageUrl] = useState(null);
+    const [file, setFile] = useState(null);
+
+    const handleFileSelect = e => {
+        setFile(e.target.files[0]);
+    };
+
+    const uploadToS3 = async () => {
+        if (!file) {
+            return;
+        }
+        const params = {
+            Bucket: AWS_BUCKET_NAME,
+            Key: `${Date.now()}.${file.name}`,
+            Body: file,
+        };
+        const { Location } = await s3.upload(params).promise();
+        setImageUrl(Location);
+        console.log("cargando a s3", Location);
+    };
+    return (
+        <div style={{ marginTop: "150px" }}>
+            <h1>Subida de archivo simple</h1>
+            <input type="file" onChange={handleFileSelect} />
+            {file && (
+                <div style={{ marginTop: "10px" }}>
+                    <button onClick={uploadToS3}>Upload</button>
+                </div>
+            )}
+            {imageUrl && (
+                <div style={{ marginTop: "10px" }}>
+                    <img src={imageUrl} alt="uploaded" />
+                </div>
+            )}
+        </div>
+    );
+};
